e3ac142aa0e1b31114b2ec1260cc2c59a11708e0
IgorOlikov <i.olikov@mail.ru>
2024-03-04 02:45:58 +0500

    init project commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+  import MyHello from "@/components/MyHello.vue";
+</script>
+
+<template>
+  lol
+  <MyHello />
+
+</template>
+
---
+++ src/components/MyHello.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>ETO COMPONENT</p>
+  <button>open</button>
+</template>
+
+<style scoped>
+
+body {
+  background: gold;
+}
+
+</style>
