0a2462617543336960745fd7bd4c108c082e6514
IgorOlikov <i.olikov@mail.ru>
2024-03-05 02:42:58 +0500

    CartItems Component

---
+++ src/components/CartItemList.vue
@@ -0,0 +1,15 @@
+<script setup>
+import CartItem from "@/components/CartItem.vue";
+
+</script>
+
+
+<template>
+  <div class="flex flex-col flex-1 gap-5 justify-between">
+    <CartItem/>
+    <CartItem/>
+    <CartItem/>
+    <CartItem/>
+    <CartItem/>
+  </div>
+</template>
